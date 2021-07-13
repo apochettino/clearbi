81c08fab1d44c9efb004782a57c3248bbfd33fa6
the.poche <a.pochettino@hotmail.com>
2021-07-13 13:48:29 -0300

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function( $ ) {
+
+  
+
+  // jQuery counterUp (used in Facts section)
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  
+
+});
