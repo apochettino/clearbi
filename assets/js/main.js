b457343ff2324ef03828905190e7a63a9d97db5f
the.poche <a.pochettino@hotmail.com>
2021-07-29 14:44:43 -0300

    newGridServices

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,32 @@
-jQuery(document).ready(function( $ ) {
-
-  
-
-  // jQuery counterUp (used in Facts section)
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
-  
-
+$('#js-flip-1').bind('click mouseleave', function() {
+  $('#js-flip-1 .card').toggleClass('flipped');
 });
+
+
+
+$('#js-flip-2').bind('click mouseleave', function() {
+  $('#js-flip-2 .card').toggleClass('flipped');
+});
+
+
+
+$('#js-flip-3').bind('click mouseleave', function() {
+  $('#js-flip-3 .card').toggleClass('flipped');
+});
+
+
+
+$('#js-flip-4').bind('click mouseleave', function() {
+  $('#js-flip-4 .card').toggleClass('flipped');
+});
+
+
+$('#js-flip-5').bind('click mouseleave', function() {
+  $('#js-flip-5 .card').toggleClass('flipped');
+});
+
+
+$('#js-flip-6').bind('click mouseleave', function() {
+  $('#js-flip-6 .card').toggleClass('flipped');
+});
+
